7e2d28a6cc948daa12d4bf865b855ab2929e24f0
fatih <bulutfatih551@gmail.com>
2020-09-18 14:13:21 +0300

    -

--- src/main/java/io/hubbox/client/RedisIOClient.java
+++ src/main/java/io/hubbox/client/RedisIOClient.java
@@ -2,33 +2,58 @@ package io.hubbox.client;
 
 import io.hubbox.listener.EventListener;
 import io.lettuce.core.RedisClient;
+import io.lettuce.core.cluster.RedisClusterClient;
+import io.lettuce.core.cluster.pubsub.StatefulRedisClusterPubSubConnection;
+import io.lettuce.core.cluster.pubsub.api.sync.RedisClusterPubSubCommands;
 import io.lettuce.core.pubsub.RedisPubSubListener;
 import io.lettuce.core.pubsub.StatefulRedisPubSubConnection;
 import io.lettuce.core.pubsub.api.sync.RedisPubSubCommands;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 public class RedisIOClient implements RedisPubSubListener<String, String> {
 
     @Getter
     @Setter
     private ClientData clientData;
+
+    /*** ========================== SINGLE STRUCTURE ========================== */
+    private RedisClient redisClient;
     private RedisPubSubCommands<String, String> publisherCommand;
     private RedisPubSubCommands<String, String> subscriberCommand;
     private StatefulRedisPubSubConnection<String, String> subConnection;
+
+    /*** ========================== CLUSTER STRUCTURE ========================== */
+    private RedisClusterClient redisClusterClient;
+    private RedisClusterPubSubCommands<String, String> publisherClusterCommand;
+    private RedisClusterPubSubCommands<String, String> subscriberClusterCommand;
+    private StatefulRedisClusterPubSubConnection<String, String> subClusterConnection;
+
     private EventListener eventListener;
 
     public RedisIOClient(RedisClient redisClient) {
+        this.redisClient = redisClient;
         this.clientData = new ClientData();
         this.subConnection = redisClient.connectPubSub();
         this.publisherCommand = redisClient.connectPubSub().sync();
     }
 
+    public RedisIOClient(RedisClusterClient redisClient) {
+        this.redisClusterClient = redisClient;
+        this.clientData = new ClientData();
+        this.subClusterConnection = redisClient.connectPubSub();
+        this.publisherClusterCommand = redisClient.connectPubSub().sync();
+    }
+
     public void addMessageListener(EventListener eventListener) {
         this.eventListener = eventListener;
-        this.subConnection.addListener(this);
-        this.subscriberCommand = subConnection.sync();
+        if (redisClient != null) {
+            this.subConnection.addListener(this);
+            this.subscriberCommand = subConnection.sync();
+        } else if (redisClusterClient != null) {
+            this.subClusterConnection.addListener(this);
+            this.subscriberClusterCommand = this.subClusterConnection.sync();
+        }
         this.subscribeToSelf();
     }
 
@@ -38,11 +63,19 @@ public class RedisIOClient implements RedisPubSubListener<String, String> {
      */
     private void subscribeToSelf() {
         System.out.println("Subscribing to self... " + clientData.getSessionId());
-        this.subscriberCommand.subscribe(clientData.getSessionId());
+        if (redisClient != null) {
+            this.subscriberCommand.subscribe(clientData.getSessionId());
+        } else if (redisClusterClient != null) {
+            this.subscriberClusterCommand.subscribe(clientData.getSessionId());
+        }
     }
 
     public void sendMessage(String message) {
-        this.publisherCommand.publish(clientData.getSessionId(), message);
+        if (redisClient != null) {
+            this.publisherCommand.publish(clientData.getSessionId(), message);
+        } else if (redisClusterClient != null) {
+            this.publisherClusterCommand.publish(clientData.getSessionId(), message);
+        }
 
     }
 
